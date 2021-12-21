f3a29f00e3903a984b3640b1e60e59fe4c0d4028
LBartolini <l.bartolini02@gmail.com>
2021-12-21 11:24:28 +0100

    esercizio java: cerca

---
+++ Cerca/src/main/Cerca.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.Random;
+
+public class Cerca {
+	/*
+	 * Definire un metodo RICORSIVO che restituisce true
+	 * se il numero x è presente nell'array a; altrimenti 
+	 * restituisce false. 
+	 */
+
+
+	static boolean cerca(int x, int[] a, int i) {
+		if(i >= a.length) return false;
+		if (a[i]==x) return true;
+		else return cerca(x, a, i+1);
+	}
+	
+	static boolean cercaRic(int x, int[] a){
+		return cerca(x, a, 0); 
+	}
+ 
+	
+	
+}
---
+++ Cerca/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] a = {1, 4, 9, 0, 3, 2, 4};
+		System.out.println(Cerca.cercaRic(5, a));
+	}
+
+}
